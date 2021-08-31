c3102105ee4dbc9425daea6b13c5cb8ee9a1195c
Michael[tm] Smith <mike@w3.org>
2021-08-31 23:05:39 +0900

    Drop payment-method-basic-card

--- .spec-urls-check.js
+++ .spec-urls-check.js
@@ -238,6 +238,10 @@ const checkSpecURL = (
    *   return;
    * }
    */
+  // FIXME temporary https://github.com/mdn/browser-compat-data/pull/12231
+  if (spec_url.startsWith('https://w3c.github.io/payment-method-basic-card/')) {
+    return;
+  }
   if (
     spec_url.startsWith('https://www.rfc-editor.org/rfc/rfc2324') ||
     spec_url.startsWith('https://www.rfc-editor.org/rfc/rfc7168')
