a8fe9d54ff9e5f05147026fbd817ee0a49e1a047
Michael[tm] Smith <mike@w3.org>
2021-09-01 16:22:23 +0900

    Add exception for MDN URL that should have spec URL

--- .spec-urls-check.js
+++ .spec-urls-check.js
@@ -124,7 +124,7 @@ const needsSpecURL = mdnURL => {
     "https://developer.mozilla.org/docs/Web/API/XRInputSourceArray/keys",
     "https://developer.mozilla.org/docs/Web/API/XRInputSourceArray/values",
     // FIXME: for this one we really do need a spec URL
-    "https://developer.mozilla.org/en-US/docs/Web/API/GamepadButton/touched",
+    "https://developer.mozilla.org/docs/Web/API/GamepadButton/touched",
     // FIXME: for this one we really do need a spec URL
     "https://developer.mozilla.org/docs/Web/HTTP/Headers/Set-Cookie/SameSite",
     // FIXME: for this one we really do need a spec URL
