a543dd54b352bab8261af2200f34e4c62d1d5962
Michael[tm] Smith <mike@w3.org>
2022-06-03 18:25:19 +0900

    Add temporary exception

--- .spec-urls-check.js
+++ .spec-urls-check.js
@@ -265,6 +265,10 @@ const checkSpecURL = (
   ) {
     return;
   }
+  // FIXME temporary https://github.com/mdn/browser-compat-data/pull/16527
+  if (spec_url.startsWith("https://www.w3.org/TR/largest-contentful-paint")) {
+    return;
+  }
   if (spec_url.startsWith("https://www.w3.org/TR/SVG11/")) {
     return;
   }
