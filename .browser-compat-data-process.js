50a5633177cc34d4480f3dd648c7851c8563edcc
Mti <Kook143125@gmail.com>
2024-09-19 04:47:27 +0700

    Update .browser-compat-data-process.js
    M

--- .browser-compat-data-process.js
+++ .browser-compat-data-process.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
-/* Any copyright is dedicated to the Public Domain.
- * http://creativecommons.org/publicdomain/zero/1.0/ */
+ Any copyright is dedicated to the Public Domain.
+ * http:"creativecommons.org/publicdomain/zero/1.0/ */
 
 "use strict";
 import chalk from "chalk";
